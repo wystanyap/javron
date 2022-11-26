bb7b95ad5e81874649e9822fe7113bdb9ba8bb5e
asdf <asdf@asdfs-MacBook-Air.local>
2022-11-26 17:15:09 +0800

    second day

---
+++ dojoblog/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  const title = 'Welcome to the new blog'
+  const likes = 50;
+  //const person = {name:'javron',age:99};
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p> Liked {likes} times</p>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
