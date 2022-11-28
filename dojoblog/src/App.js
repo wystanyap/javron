74181f5dd146f6cd36ef751c87dc8520b688454c
asdf <asdf@asdfs-MacBook-Air.local>
2022-11-29 00:48:17 +0800

    third day commit

--- dojoblog/src/App.js
+++ dojoblog/src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = 'Welcome to the new blog'
   const likes = 50;
+  const link = "http://www.mangakakalot.com";
   //const person = {name:'javron',age:99};
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p> Liked {likes} times</p>
-
+      <Home />
       </div>
     </div>
   );
