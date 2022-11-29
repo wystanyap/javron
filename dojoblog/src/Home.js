7d71d114eed9e5c30786eb037cdf8b461a42fde4
asdf <asdf@asdfs-MacBook-Air.local>
2022-11-30 05:29:29 +0800

    forth day functions and hooks

--- dojoblog/src/Home.js
+++ dojoblog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, Javron')
+    }
+
+    const clickAgain = (name, e) => {
+        console.log('Hello' + name, e.target);
+    }
+
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => {clickAgain('jabol',e);}}>Click Again</button>
         </div>
      );
 }
